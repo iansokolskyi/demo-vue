e45818b68bb0b1e7ac6ad597246e276b69286b26
Ian Sokolskyi <iansokolskyi@gmail.com>
2021-05-06 15:28:39 +0300

    set publicPath in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
+const PORT = process.env.PORT || 5000;
 module.exports = {
     devServer: {
-        disableHostCheck: true
-    }
+        disableHostCheck: true,
+        port: PORT,
+        public: `0.0.0.0:${PORT}`
+    },
+    publicPath: "/"
 }
